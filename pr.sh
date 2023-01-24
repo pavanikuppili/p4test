23ff866e51cfd05d228ec4f04bd4e4a896e884f1
Pavani Kuppili <41535888+PavaniKRao@users.noreply.github.com>
2023-01-23 18:03:56 -0700

    Delete pr.sh

--- pr.sh
+++
@@ -1,2 +0,0 @@
-#! /bin/sh
-sudo mv prnext.sh ../../root/ && sudo apt-get update && sudo apt-get install libxml2-dev pkg-config && sudo git -C ../../root/ clone https://gitlab.flux.utah.edu/amaricq/SLANG-probed.git && sudo chmod +x ../../root/prnext.sh && sudo /../../root/prnext.sh &&  sudo -i
