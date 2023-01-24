519b5ddd3ea2604b9b0d1c205c009fa1e2f492b5
Pavani Kuppili <41535888+PavaniKRao@users.noreply.github.com>
2023-01-23 20:56:57 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -16,10 +16,7 @@ node2 = request.RawPC("node2")
 link = request.Link(members = [node1, node2])
 
 # Install and execute a script that is contained in the repository.
-node1.addService(pg.Execute(shell="sh", command="sudo apt-get update -y && sudo apt-get install libxml2-dev pkg-config -y && sudo git -C /root/ clone https://gitlab.flux.utah.edu/amaricq/SLANG-probed.git && sudo autoreconf -i /root/SLANG-probed/ && sudo /root/SLANG-probed/configure && sudo make -C /root/SLANG-probed/")) 
-#node1.addService(pg.Execute(shell="sh", command="sudo -i && cd /root/SLANG-probed && autoreconf -i && ./configure && make"))
-#node2.addService(pg.Execute(shell="sh", command="sudo apt-get update && sudo apt-get install libxml2-dev pkg-config && sudo git -C /root/ clone https://gitlab.flux.utah.edu/amaricq/SLANG-probed.git && sudo -i")) 
-#node2.addService(pg.Execute(shell="sh", command="cd /root/SLANG-probed && autoreconf -i && ./configure && make"))
+node1.addService(pg.Execute(shell="sh", command="sudo apt-get update -y && sudo apt-get install libxml2-dev pkg-config -y && sudo git -C /root/ clone https://gitlab.flux.utah.edu/amaricq/SLANG-probed.git && sudo autoreconf -i /root/SLANG-probed/ && sudo /root/SLANG-probed/configure")) 
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
