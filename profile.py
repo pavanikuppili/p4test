8eda2ed5b1f97a100536427f66ac9666599aa592
Pavani Kuppili <41535888+PavaniKRao@users.noreply.github.com>
2023-01-23 19:42:36 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -13,6 +13,8 @@ request = pc.makeRequestRSpec()
 node1 = request.RawPC("node1")
 node2 = request.RawPC("node2")
 
+link1 = request.Link(members = [node1, node2])
+
 # Install and execute a script that is contained in the repository.
 node1.addService(pg.Execute(shell="sh", command="/local/repository/pr.sh"))
 node2.addService(pg.Execute(shell="sh", command="/local/repository/pr.sh"))
