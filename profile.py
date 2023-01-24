cb11f3ac87c7c9a7850ce4c3bb4c88b779c4bedc
Pavani Kuppili <41535888+PavaniKRao@users.noreply.github.com>
2023-01-23 19:05:14 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -10,7 +10,8 @@ node2 = request.RawPC("node2")
 
 link1 = request.Link(members = [node1, node2])
 
-node.addService(rspec.Install(url="https://github.com/PavaniKRao/p4test/blob/main/pr.sh", path="/root"))
+node1.addService(rspec.Install(url="https://github.com/PavaniKRao/p4test/blob/main/pr.sh", path="/root"))
+node2.addService(rspec.Install(url="https://github.com/PavaniKRao/p4test/blob/main/pr.sh", path="/root"))
 #node1.addService(rspec.Execute(shell="sh", command="/local/repository/pr.sh"))
 #node2.addService(rspec.Execute(shell="sh", command="/local/repository/pr.sh"))
 
