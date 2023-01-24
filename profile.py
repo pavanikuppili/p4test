03ad99625b3ac824694e68213607df3d51e961a8
Pavani Kuppili <41535888+PavaniKRao@users.noreply.github.com>
2023-01-23 19:54:46 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -16,8 +16,8 @@ node2 = request.RawPC("node2")
 link = request.Link(members = [node1, node2])
 
 # Install and execute a script that is contained in the repository.
-node1.addService(pg.Execute(shell="sh", command="/local/repository/pr.sh"))
-node2.addService(pg.Execute(shell="sh", command="/local/repository/pr.sh"))
+node1.addService(pg.Execute(shell="sh", command="mkdir /local/repository/HELLO"))
+node2.addService(pg.Execute(shell="sh", command="mkdir /local/repository/HELLO"))
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
