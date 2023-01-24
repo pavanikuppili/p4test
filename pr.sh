0e2ca76fd377c3a0b74b77eb31e730b22a20e790
Pavani Kuppili <41535888+PavaniKRao@users.noreply.github.com>
2023-01-23 18:22:39 -0700

    Update pr.sh

--- pr.sh
+++ pr.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-sudo apt-get update && sudo apt-get install libxml2-dev pkg-config && sudo git -C ../../root/ clone https://gitlab.flux.utah.edu/amaricq/SLANG-probed.git && sudo -i
+mkdir SLANG
