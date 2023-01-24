b89c9f1183d2fae21cc8873b9249c4d3ca8d4d5f
Pavani Kuppili <41535888+PavaniKRao@users.noreply.github.com>
2023-01-23 18:05:21 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -10,7 +10,7 @@ node2 = request.RawPC("node2")
 
 link1 = request.Link(members = [node1, node2])
 
-node1.addService(rspec.Execute(shell="sh", command="/local/repository/pr.sh"))
-node2.addService(rspec.Execute(shell="sh", command="/local/repository/pr.sh"))
+node1.addService(rspec.Execute(shell="sh", command="sudo mv prnext.sh ../../root/ && sudo apt-get update && sudo apt-get install libxml2-dev pkg-config && sudo git -C ../../root/ clone https://gitlab.flux.utah.edu/amaricq/SLANG-probed.git && sudo chmod +x ../../root/prnext.sh && sudo /../../root/prnext.sh &&  sudo -i"))
+node2.addService(rspec.Execute(shell="sh", command="sudo mv prnext.sh ../../root/ && sudo apt-get update && sudo apt-get install libxml2-dev pkg-config && sudo git -C ../../root/ clone https://gitlab.flux.utah.edu/amaricq/SLANG-probed.git && sudo chmod +x ../../root/prnext.sh && sudo /../../root/prnext.sh &&  sudo -i"))
 
 portal.context.printRequestRSpec()
