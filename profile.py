268fdbda97475f122a9b507330524382ac675ac7
Pavani Kuppili <41535888+PavaniKRao@users.noreply.github.com>
2023-01-23 21:12:41 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -16,7 +16,8 @@ node2 = request.RawPC("node2")
 link = request.Link(members = [node1, node2])
 
 # Install and execute a script that is contained in the repository.
-node1.addService(pg.Execute(shell="sh", command="sudo apt-get update -y && sudo apt-get install libxml2-dev pkg-config -y && sudo git -C /root/ clone https://gitlab.flux.utah.edu/amaricq/SLANG-probed.git && sudo autoreconf -i /root/SLANG-probed/ && sudo /root/SLANG-probed/configure && sudo make")) 
+node1.addService(pg.Execute(shell="sh", command="sudo apt-get update -y && sudo apt-get install libxml2-dev pkg-config -y && sudo git -C /root/ clone https://gitlab.flux.utah.edu/amaricq/SLANG-probed.git && sudo autoreconf -i /root/SLANG-probed/ && sudo /root/SLANG-probed/configure && sudo make"))
+node2.addService(pg.Execute(shell="sh", command="sudo apt-get update -y && sudo apt-get install libxml2-dev pkg-config -y && sudo git -C /root/ clone https://gitlab.flux.utah.edu/amaricq/SLANG-probed.git && sudo autoreconf -i /root/SLANG-probed/ && sudo /root/SLANG-probed/configure && sudo make")) 
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
