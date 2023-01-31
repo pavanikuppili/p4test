c375b9661b9a9d5764e0bb604e628919a68cd6c9
Pavani Kuppili <41535888+PavaniKRao@users.noreply.github.com>
2023-01-31 13:29:20 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -27,12 +27,12 @@ node2.hardware_type = 'xl170'
 link = request.Link(members = [node1, node2])
 
 # Install and execute a script that is contained in the repository.
-node1.addService(pg.Execute(shell="sh", command="sudo apt-get update -y && sudo apt-get install libxml2-dev pkg-config -y && sudo git -C /root/ clone https://gitlab.flux.utah.edu/amaricq/SLANG-probed.git && sudo autoreconf -i /root/SLANG-probed/ "))
-node1.addService(pg.Execute(shell="sh", command="sudo bash -c 'cd /root/SLANG-probed ; ./configure ; make'"))
-#node1.addService(pg.Execute(shell="sh", command="sudo bash -c 'cp /root/SLANG-probed/probed /usr/local/bin/'"))
+node1.addService(pg.Execute(shell="sh", command="sudo apt-get update -y && sudo apt-get install libxml2-dev pkg-config -y && sudo git -C /root/ clone https://github.com/itsiprikshit/probed.git && sudo autoreconf -i /root/probed/ "))
+node1.addService(pg.Execute(shell="sh", command="sudo bash -c 'cd /root/probed ; ./configure ; make'"))
+node1.addService(pg.Execute(shell="sh", command="sudo bash -c 'cp /root/probed/probed /usr/local/bin/'"))
 
-node2.addService(pg.Execute(shell="sh", command="sudo apt-get update -y && sudo apt-get install libxml2-dev pkg-config -y && sudo git -C /root/ clone https://gitlab.flux.utah.edu/amaricq/SLANG-probed.git && sudo autoreconf -i /root/SLANG-probed/ "))
-node2.addService(pg.Execute(shell="sh", command="sudo bash -c 'cd /root/SLANG-probed ; ./configure ; make'"))
+#node2.addService(pg.Execute(shell="sh", command="sudo apt-get update -y && sudo apt-get install libxml2-dev pkg-config -y && sudo git -C /root/ clone https://gitlab.flux.utah.edu/amaricq/SLANG-probed.git && sudo autoreconf -i /root/SLANG-probed/ "))
+#node2.addService(pg.Execute(shell="sh", command="sudo bash -c 'cd /root/SLANG-probed ; ./configure ; make'"))
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
