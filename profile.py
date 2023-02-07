3818021282c7e773a89e75a2afe018b90e4268ef
Pavani Kuppili <41535888+PavaniKRao@users.noreply.github.com>
2023-02-06 18:54:16 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -24,7 +24,8 @@ node2 = request.RawPC("node2")
 node1.hardware_type = 'xl170'
 node2.hardware_type = 'xl170'
 
-link = request.Link(members = [node1, node2])
+#link = request.Link(members = [node1, node2])
+link = request.Link(ltype = "L2", members = [node1, node2])
 
 # Install and execute a script that is contained in the repository.
 node1.addService(pg.Execute(shell="sh", command="sudo apt-get update -y && sudo apt-get install libxml2-dev pkg-config -y && sudo git -C /root/ clone https://github.com/itsiprikshit/probed.git && sudo autoreconf -i /root/probed/ "))
