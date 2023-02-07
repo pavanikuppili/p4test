eeb09b14e212c17e6ce26933b85b5dd211cf1152
Pavani Kuppili <41535888+PavaniKRao@users.noreply.github.com>
2023-02-07 13:31:51 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -25,7 +25,11 @@ node1.hardware_type = 'xl170'
 node2.hardware_type = 'xl170'
 
 #link = request.Link(members = [node1, node2])
-link = request.Link(ltype = "L2", members = [node1, node2])
+link_0 = request.Link(ltype = "L2", members = [node1, node2])
+link_1 = request.Link(ltype = "L1", members = [node1, node2])
+
+link_0.setNoInterSwitchLinks()
+#link_1.setNoInterSwitchLinks()
 
 # Install and execute a script that is contained in the repository.
 node1.addService(pg.Execute(shell="sh", command="sudo apt-get update -y && sudo apt-get install libxml2-dev pkg-config -y && sudo git -C /root/ clone https://github.com/itsiprikshit/probed.git && sudo autoreconf -i /root/probed/ "))
