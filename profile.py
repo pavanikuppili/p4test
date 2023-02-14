4f0e4709655040f45e7242adba7b497619191cb3
Pavani Kuppili <41535888+PavaniKRao@users.noreply.github.com>
2023-02-13 23:21:56 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -26,26 +26,27 @@ node2.hardware_type = 'xl170'
 node1.component_id = urn.Node(cloudlab.Utah, "hp197")
 node2.component_id = urn.Node(cloudlab.Utah, "hp199")
 
-#iface1 = node1.addInterface()
-#iface2 = node1.addInterface()
-#iface3 = node2.addInterface()
-#iface4 = node2.addInterface()
+iface1 = node1.addInterface()
+iface2 = node1.addInterface()
+iface3 = node2.addInterface()
+iface4 = node2.addInterface()
 
 
 #link = request.Link(members = [node1, node2])
 #link_0 = request.Link(ltype = "L2", members = [node1, node2])
-link_1 = request.Link(ltype = "L1", members = [node1, node2])
+#link_1 = request.Link(ltype = "L1", members = [node1, node2])
 
 
-link_1.setNoInterSwitchLinks()
-#link_0 = request.Link(ltype = "L2")
-#link_0.addInterface(iface1)
-#link_0.addInterface(iface3)
+
+link_0 = request.Link(ltype = "L2")
+link_0.addInterface(iface1)
+link_0.addInterface(iface3)
 #link_0.setNoInterSwitchLinks()
 
-#link_1 = request.Link()
-#link_1.addInterface(iface2)
-#link_1.addInterface(iface4)
+link_1 = request.Link(ltype = "L1")
+link_1.addInterface(iface2)
+link_1.addInterface(iface4)
+link_1.setNoInterSwitchLinks()
 
 #node1.component_id = urn.Node(cloudlab.Utah, "hp197")
 #node2.component_id = urn.Node(cloudlab.Utah, "hp199")
