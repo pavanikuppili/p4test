2e186a69fb1cb18b9b5a6cdf6c8b386ec6f4217e
Pavani Kuppili <41535888+PavaniKRao@users.noreply.github.com>
2023-02-13 21:47:25 -0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -34,7 +34,7 @@ iface4 = node2.addInterface()
 
 #link = request.Link(members = [node1, node2])
 link_0 = request.Link(ltype = "L2", members = [node1, node2])
-link_1 = request.Link(ltype = "L1", members = [node1, node2])
+#link_1 = request.Link(ltype = "L1", members = [node1, node2])
 
 link_0.setNoInterSwitchLinks()
 #link_1.setNoInterSwitchLinks()
@@ -42,8 +42,8 @@ link_0.setNoInterSwitchLinks()
 link_0.addInterface(iface1)
 link_0.addInterface(iface3)
 
-link_1.addInterface(iface2)
-link_1.addInterface(iface4)
+#link_1.addInterface(iface2)
+#link_1.addInterface(iface4)
 
 #node1.component_id = urn.Node(cloudlab.Utah, "hp197")
 #node2.component_id = urn.Node(cloudlab.Utah, "hp199")
